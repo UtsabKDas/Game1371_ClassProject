a7b555c29e0702e1666b49b2ab95f2d1da67b395
UtsabKDas <37959749+UtsabKDas@users.noreply.github.com>
2026-09-16 23:12:00 -0500

    Add the Initial Dungeon Game
    Add a Scene with a GameManager that has the DungeonGame.cs script
    Add the DungeonGame.cs script with basic Debug.Log outputs.

---
+++ Assets/Scripts/DungeonGame/DungeonGame.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class DungeonGame : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("==== DUNGEON GAME ====");
+        Debug.Log("Welcome, hero. Your escape begins.");
+
+        Debug.Log("");
+
+        Debug.Log("The Entrance Hall");
+        Debug.Log("A torch flickers on the wall. A stone doorway leads north.");
+        Debug.Log("You move into the next room");
+
+        Debug.Log("");
+
+        Debug.Log("The Guard Room");
+        Debug.Log("A rusty sword rests on a table. A goblin snores in the corner");
+        Debug.Log("You move into the next room");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
