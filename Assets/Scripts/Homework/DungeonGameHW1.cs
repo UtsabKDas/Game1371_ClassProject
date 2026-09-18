746715b7cf267f7d02b81c47050607f35d2a7e8b
UtsabKDas <37959749+UtsabKDas@users.noreply.github.com>
2026-09-17 20:35:11 -0500

    Solution to Homework 3: Dungeon Game

--- Assets/Scripts/Homework/DungeonGameHW1.cs
+++ Assets/Scripts/Homework/DungeonGameHW1.cs
@@ -18,6 +18,18 @@ public class DungeonGameHW1 : MonoBehaviour
         //            agility (int), gold (int), hasKey (bool),
         //            goblinHealth (int), goblinAttack (int).
 
+        string playerName = "Alfred";
+        int health = 100;
+        int attack = 25;
+        int defense = 5;
+        int agility = 10;
+        int gold = 1000;
+        bool hasKey = true;
+
+        int goblinHealth = 10;
+        int goblinAttack = 5;
+        int goblinDefense = 2;
+
         // ===== ALREADY BUILT IN CLASS (Intro lecture): the opening + two rooms =====
         Debug.Log("=== THE DUNGEON ===");
         Debug.Log("Welcome, Hero. Your escape begins."); // replace Hero with the name of your player.
@@ -29,7 +41,15 @@ public class DungeonGameHW1 : MonoBehaviour
 
         Debug.Log("");
         Debug.Log("The Guard Room");
+        int rustySwordAttack = 5;
         Debug.Log("A rusty sword rests on a table. A goblin snores in the corner.");
+        attack += rustySwordAttack;
+
+        Debug.Log("Goblin wakes up and attacks");
+        health -= (goblinAttack - defense);
+        Debug.Log("You have taken " + goblinAttack + " amount of damage. You now have " + health + " health");
+        goblinHealth -= (attack - goblinDefense);
+        Debug.Log("You attack the goblin for " + attack + " amount of damage. The goblin now has " + goblinHealth + " health");
         Debug.Log("You move into the next room.");
 
         // ======================================================================
@@ -39,15 +59,31 @@ public class DungeonGameHW1 : MonoBehaviour
         // TODO A1: FIX THE BROKEN ROOM below. It has bugs that stops the program
         //          from running. Un-comment the lines, find the bug(s), fix it,
         //          and add a // comment saying what was wrong.
-        // Debug.Log("The Flooded Passage")
-        // Debug.Log("Ankle-deep water fills the hall. A broken door is at the end of the hallway.");
-        // Debug.Log("You move into the next room.");
+        Debug.Log("The Flooded Passage");
+        int trapDamage = 10;
+        Debug.Log("Ankle-deep water fills the hall. A broken door is at the end of the hallway.");
+        Debug.Log("A pirahna bites you underwater. Take " + trapDamage + " damage");
+        health -= trapDamage;
+        Debug.Log("You move into the next room.");
 
         // TODO A2: write at least one of your OWN room - a Room Name line,
         //          a description line, and a line describing how you exit. 
+        Debug.Log("My Own Room");
+        Debug.Log("Here's a description of my room.");
+        Debug.Log("Get out.");
+
+        Debug.Log("Merchant Room");
+        int potionCost = 7;
+        Debug.Log("Some sketchy dude in a trench coat has stuff for you. He's selling potions at " + potionCost + " gold each");
+        Debug.Log("You can buy " + gold / potionCost + " and you would have " + gold % potionCost + " gold left over");
+        Debug.Log("Get out.");
+
 
         // TODO A3: write the EXIT room - a final "room" and description that leads the
         //          player out of the dungeon.
+        Debug.Log("Dungeon Exit");
+        Debug.Log("You see a light at the end of a long tunnel. Hope it's not death");
+        Debug.Log("You exit the dungeon");
 
         // ======================================================================
         // PART B  -  after the VARIABLES lecture (variables & operators)
@@ -56,10 +92,11 @@ public class DungeonGameHW1 : MonoBehaviour
         // TODO B2: FIX THE BROKEN ROOM below. It has bugs that stops the program
         //          from running. Un-comment the lines, find the bug(s), fix it,
         //          and add a // comment saying what was wrong.
-        // int gold = 15;
-        // Debug.Log(The Treasure Room);
-        // Debug.Log("It seems this room has been raided. You find " + 5000 + " gold.");
-        // Debug.Log("You move into the next room.)
+        int goldInRoom = 5000;
+        Debug.Log("The Treasure Room");
+        Debug.Log("It seems this room has been raided. You find " + goldInRoom + " gold.");
+        gold += goldInRoom;
+        Debug.Log("You move into the next room.");
 
         // TODO B3: Go back through your rooms above and add an event/item to each
         //          one that changes a stat/variable, printing the new value right
